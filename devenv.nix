25fccf6e211db3649d85edf6f4044a3b5441fd69
Alejandro Gomez <alejandro@meetiqm.com>
2025-01-05 10:46:53 +0200

    Simplify setup

--- devenv.nix
+++ devenv.nix
@@ -1,45 +1,29 @@
-{ pkgs, lib, config, inputs, ... }:
-
 {
- packages= [ pkgs.git ];
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  packages = [pkgs.git];
   languages.python.enable = true;
   languages.python.uv.enable = true;
   languages.python.uv.sync.enable = true;
   languages.python.uv.sync.allExtras = true;
   languages.python.venv.enable = true;
-  #
-  enterShell = ''
-    hello
-    git --version
-  '';
 
-  # https://devenv.sh/tasks/
- scripts.pytest.exec = ''
+  scripts.pytest.exec = ''
     pytest
   '';
-  # tasks = {
-  #     "myproj:"
-    # "myproj:setup".exec = "mytool build";
-    # "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
 
   pre-commit = {
-      hooks = {
-          # mypy.enable = true;
-          ruff.enable = true;
-          ruff-format.enable = true;
-
-      };
+    hooks = {
+      # mypy.enable = true;
+      ruff.enable = true;
+      ruff-format.enable = true;
+    };
   };
-  # https://devenv.sh/tests/
   enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
     pytest
   '';
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Development environment for devto.py";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv = {
+      url = "github:cachix/devenv";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;}
+    {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        devenv.shells.default = {
+          imports = [./devenv.nix];
+        };
+      };
+    };
+}
